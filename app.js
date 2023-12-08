310f5456395e827001b1f24d43944d02831682b9
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-12-08 14:51:57 +0300

    add sec baeane ha
    add html css statik no js

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-// events 
-document.addEventListener("afterload",afterload())
-// function 
+// events
+document.addEventListener("afterload", afterload());
+// function
 // function afterload
 function afterload() {
-    
-    for (let i = 0; i < 6841 ; i++) {
+    this.dramad()
+}
+function dramad() {
+    for (let i = 0; i < 6841; i++) {
         setTimeout(() => {
-            let dramade = document.querySelector(".box-cash-body-dramad-part:first-of-type").children[0].innerHTML = "$" +i
-            if (i  < 81){
-                let tavize = document.querySelector(".box-cash-body-dramad-part:nth-child(2)").children[0].innerHTML = i + "%"
-            }
-            if (i  < 1241){
-                let tavize = document.querySelector(".box-cash-body-dramad-part:last-of-type").children[0].innerHTML = "$" +i 
-            }
-        }, 7000);
-    }
+          let dramade = (document.querySelector(
+            ".box-cash-body-dramad-part:first-of-type"
+          ).children[0].innerHTML = "$" + i);
+          if (i < 81) {
+            let tavize = (document.querySelector(
+              ".box-cash-body-dramad-part:nth-child(2)"
+            ).children[0].innerHTML = i + "%");
+          }
+          if (i < 1241) {
+            let hazine = (document.querySelector(
+              ".box-cash-body-dramad-part:last-of-type"
+            ).children[0].innerHTML = "$" + i);
+          }
+        }, 300);
+      }
 }
