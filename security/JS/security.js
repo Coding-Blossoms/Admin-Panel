d25418d725f8b9f7ca5d3d5f2f12eb1cea58d230
MaHsa/ ✓ <127294908+Mahsa-Arabzadeh@users.noreply.github.com>
2023-12-10 17:13:36 +0330

    Add style changes.

--- security/JS/security.js
+++ security/JS/security.js
@@ -1,3 +1,21 @@
 import { Table } from "../components/Table/table.js";
 
 window.customElements.define("table-chart", Table);
+
+const copyButton = document.querySelector(".copy-btn");
+copyButton.addEventListener("click", copyBtn);
+
+function copyBtn() {
+  let textSelect = new Table();
+  console.log(textSelect);
+  let text = document.querySelector(".text-table-one2").innerHTML;
+  const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Content copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+  return copyContent;
+}
