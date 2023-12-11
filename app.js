7d866e5e7bf7a05be3daacc8fe0f77fd1f89c138
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-12-11 13:58:49 +0300

    add part cash static

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // function
 function copyLinkSECBarnameArga() {
   let urlSECBarnameArga = document.querySelector("#urlInput")
-
   urlSECBarnameArga.select();
   urlSECBarnameArga.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(urlSECBarnameArga.value);
