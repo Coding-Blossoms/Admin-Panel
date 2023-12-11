186030ad458ca7c285f40607a116c27986ed68f3
MaHsa/ ✓ <127294908+Mahsa-Arabzadeh@users.noreply.github.com>
2023-12-11 14:08:53 +0330

    Add function (copyBtn) copy to the clipboard.

--- security/JS/security.js
+++ security/JS/security.js
@@ -4,9 +4,13 @@ window.customElements.define("table-chart", Table);
 
 const copyButton = document.querySelector(".copy-btn");
 copyButton.addEventListener("click", copyBtn);
+const copy = document.querySelector("#text-table-one2");
 
 function copyBtn() {
-  const table = document.querySelector("#table-chart5");
-  const tableAttribute = table.getAttribute("text-table-one2");
-  console.log(tableAttribute);
+  copy.select();
+  document.execCommand("copy");
+  copy.style.color = "#50cd89";
+  setInterval(() => {
+    copy.style.color = "#6d6d80";
+  }, 4000);
 }
