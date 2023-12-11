8b5e2e30da514df80b15ec1ee1387631eac0fc11
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-12-11 16:31:26 +0300

    add dramd part

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+// event
+addEventListener('load', afterLoad)
+
 // function
+function afterLoad(){
+  dramad()
+}
 function copyLinkSECBarnameArga() {
   let urlSECBarnameArga = document.querySelector("#urlInput")
   urlSECBarnameArga.select();
   urlSECBarnameArga.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(urlSECBarnameArga.value);
+}
+function dramad(){
+  let number = 0
+  for (let i = 0; i < 63240; i++) {
+    setTimeout(() => {
+      number += 1  
+      let dramade = (document.querySelector(
+        "section:first-of-type>div:nth-of-type(2) > div > h3"
+      ).innerHTML = "$" + number);
+      if (i <  8530) {
+        let tavize = (document.querySelector(
+          "section:first-of-type>div:nth-of-type(2) > div:nth-of-type(2) > h3"
+        ).innerHTML ="$" + number );
+      }
+      if (i < 2600) {
+        let hazine = (document.querySelector(
+          "section:first-of-type>div:nth-of-type(2) > div:nth-of-type(3) > h3"
+        ).innerHTML = "$" + number);
+      }
+      if (i < 783) {
+        let hazine = (document.querySelector(
+          "section:first-of-type>div:nth-of-type(2) > div:nth-of-type(4) > h3"
+        ).innerHTML = "$" + number);
+      }
+    }, 1300);
+  }
 }
