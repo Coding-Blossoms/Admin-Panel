8bd05f43ba0b616e13a928206ac5c9e1b7b4473d
MaHsa/ ✓ <127294908+Mahsa-Arabzadeh@users.noreply.github.com>
2023-12-12 06:51:07 +0330

    Add swiper slide (swiperjs library).

---
+++ security/JS/library/custom.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+export { swiper };
